b027aac8362e677e3ee338721481833a3636300b
MatthewPetroske <Matthew.Petroske@cwu.edu>
2016-11-02 18:26:01 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <QCoreApplication>
 
-int main()
+int main(int argc, char *argv[])
 {
-  std::cout << "Hello World!";
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
 }
